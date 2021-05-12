1ffa8ab583a2972f020cb1e3948f57e8e4aa1efa
Robert Lord <lord@users.noreply.github.com>
2021-05-11 21:42:29 -0500

    bevy beginner example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use bevy::prelude::*;
+
+struct Person;
+struct Name(String);
+
 fn main() {
-    println!("Hello, world!");
+    // hello
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(add_people.system())
+        .add_system(greet_people.system())
+        .add_system(hello_world.system())
+        .run();
+}
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
+}
+
+fn hello_world() {
+    // println!("hello world!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        // println!("hello {}!", name.0);
+    }
 }
